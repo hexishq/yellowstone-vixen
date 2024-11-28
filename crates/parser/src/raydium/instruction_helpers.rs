9d5398cef0a952171d5ecebf443697e7b1268fb6
Frn-sz <fernandogdemedeirosw@gmail.com>
2024-11-28 16:45:01 -0300

    feat: add instruction types and discriminators

--- crates/parser/src/raydium/instruction_helpers.rs
+++ crates/parser/src/raydium/instruction_helpers.rs
@@ -1,15 +1,53 @@
-use solana_program::{pubkey, pubkey::Pubkey};
+use borsh::BorshDeserialize;
 
-pub const RAYDIUM_AMM_V4_POOL: Pubkey = pubkey!("675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8");
+pub const DISCRIMINATOR_SIZE: usize = 8;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, BorshDeserialize)]
 pub enum RaydiumInstruction {
     SwapBaseIn(SwapBaseIn),
     SwapBaseOut(SwapBaseOut),
+    Withdraw(Withdraw),
+    Deposit(Deposit),
+    Initialize2(Initialize2),
 }
 
-#[derive(Debug, Clone, Copy)]
-pub struct SwapBaseIn {}
+pub const SWAP_BASE_IN_IX_DISC: [u8; 8] = [9, 0, 0, 0, 0, 0, 0, 0];
+#[derive(Debug, Clone, Copy, BorshDeserialize)]
+pub struct SwapBaseIn {
+    pub amount_in: u64,
+    pub minimum_amount_out: u64,
+}
 
-#[derive(Debug, Clone, Copy)]
-pub struct SwapBaseOut {}
+pub const SWAP_BASE_OUT_IX_DISC: [u8; 8] = [11, 0, 0, 0, 0, 0, 0, 0];
+#[derive(Debug, Clone, Copy, BorshDeserialize)]
+pub struct SwapBaseOut {
+    pub max_amount_in: u64,
+    pub amount_out: u64,
+}
+
+pub const WITHDRAW_IX_DISC: [u8; 8] = [4, 0, 0, 0, 0, 0, 0, 0];
+#[derive(Debug, Clone, Copy, BorshDeserialize)]
+pub struct Withdraw {
+    pub amount: u64,
+    pub min_coin_amount: Option<u64>,
+    pub min_pc_amount: Option<u64>,
+}
+
+pub const DEPOSIT_IX_DISC: [u8; 8] = [3, 0, 0, 0, 0, 0, 0, 0];
+
+#[derive(Debug, Clone, Copy, BorshDeserialize)]
+pub struct Deposit {
+    pub max_coin_amount: u64,
+    pub max_pc_amount: u64,
+    pub base_side: u64,
+    pub other_amount_min: Option<u64>,
+}
+
+pub const INITIALIZE2_IX_DISC: [u8; 8] = [1, 0, 0, 0, 0, 0, 0, 0];
+#[derive(Debug, Clone, Copy, BorshDeserialize)]
+pub struct Initialize2 {
+    pub nonce: u8,
+    pub open_time: u64,
+    pub init_pc_amount: u64,
+    pub init_coin_amount: u64,
+}
