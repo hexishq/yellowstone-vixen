e622012edccbf139695eaf4c2501cd683c15d307
Frn-sz <fernandogdemedeirosw@gmail.com>
2024-11-28 16:46:05 -0300

    feat: add parse method

--- crates/parser/src/raydium/instruction_parser.rs
+++ crates/parser/src/raydium/instruction_parser.rs
@@ -1,14 +1,19 @@
-use yellowstone_grpc_proto::geyser::SubscribeUpdateTransaction;
-use yellowstone_vixen_core::Parser;
+use borsh::BorshDeserialize;
+use solana_program::{pubkey, pubkey::Pubkey};
+use yellowstone_vixen_core::{instruction::InstructionUpdate, ParseResult, Parser};
 
-use super::instruction_helpers::{RaydiumInstruction, RAYDIUM_AMM_V4_POOL};
+pub const RAYDIUM_AMM_V4_POOL: Pubkey = pubkey!("675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8");
+
+use crate::helpers::IX_DISCRIMINATOR_SIZE;
+
+use super::instruction_helpers::*;
 
 pub struct InstructionParser;
 
 impl Parser for InstructionParser {
-    type Input = SubscribeUpdateTransaction;
-
+    type Input = InstructionUpdate;
     type Output = RaydiumInstruction;
+
     fn id(&self) -> std::borrow::Cow<str> {
         "yellowstone_vixen_parser::raydium_amm_v4::InstructionParser".into()
     }
@@ -20,11 +25,47 @@ impl Parser for InstructionParser {
             .unwrap()
     }
 
-    fn parse(
-        &self,
-        value: &Self::Input,
-    ) -> impl std::future::Future<Output = yellowstone_vixen_core::ParseResult<Self::Output>> + Send
-    {
-        todo!()
+    async fn parse(&self, value: &InstructionUpdate) -> ParseResult<Self::Output> {
+        if value.program.equals_ref(RAYDIUM_AMM_V4_POOL) {
+            InstructionParser::parse_impl(value).map_err(|e| e.into())
+        } else {
+            Err(yellowstone_vixen_core::ParseError::Filtered)
+        }
+    }
+}
+
+impl InstructionParser {
+    pub(crate) fn parse_impl(
+        ix: &InstructionUpdate,
+    ) -> Result<RaydiumInstruction, yellowstone_vixen_core::ParseError> {
+        let ix_discriminator: [u8; 8] = ix.data[0..IX_DISCRIMINATOR_SIZE].try_into()?;
+        let mut ix_data = &ix.data[IX_DISCRIMINATOR_SIZE..];
+
+        match ix_discriminator {
+            SWAP_BASE_IN_IX_DISC => {
+                let swap_base_in: SwapBaseIn = BorshDeserialize::deserialize(&mut ix_data).unwrap();
+                Ok(RaydiumInstruction::SwapBaseIn(swap_base_in))
+            },
+            SWAP_BASE_OUT_IX_DISC => {
+                let swap_base_out: SwapBaseOut =
+                    BorshDeserialize::deserialize(&mut ix_data).unwrap();
+                Ok(RaydiumInstruction::SwapBaseOut(swap_base_out))
+            },
+            WITHDRAW_IX_DISC => {
+                let withdraw: Withdraw = BorshDeserialize::deserialize(&mut ix_data).unwrap();
+                Ok(RaydiumInstruction::Withdraw(withdraw))
+            },
+            DEPOSIT_IX_DISC => {
+                let deposit: Deposit = BorshDeserialize::deserialize(&mut ix_data).unwrap();
+                Ok(RaydiumInstruction::Deposit(deposit))
+            },
+            INITIALIZE2_IX_DISC => {
+                let initialize2: Initialize2 = BorshDeserialize::deserialize(&mut ix_data).unwrap();
+                Ok(RaydiumInstruction::Initialize2(initialize2))
+            },
+            _ => Err(yellowstone_vixen_core::ParseError::Other(
+                "Unknown instruction".into(),
+            )),
+        }
     }
 }
