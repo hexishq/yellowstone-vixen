6d8a330f19ad60b4b04749a0442be4e1ad2459b1
Frn-sz <fernandogdemedeirosw@gmail.com>
2024-11-28 15:43:58 -0300

    feat: add RAYDIUM_v4 Pubkey const

--- crates/parser/src/raydium/instruction_helpers.rs
+++ crates/parser/src/raydium/instruction_helpers.rs
@@ -1,4 +1,6 @@
-pub const RAYDIUM_AMM_V4_POOL: &str = "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8";
+use solana_program::{pubkey, pubkey::Pubkey};
+
+pub const RAYDIUM_AMM_V4_POOL: Pubkey = pubkey!("675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8");
 
 #[derive(Debug, Clone, Copy)]
 pub enum RaydiumInstruction {
